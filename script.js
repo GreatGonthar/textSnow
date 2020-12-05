73893bdddda218bf82f24b793d3159239d0671bd
GreatGonthar <gontharforeverrules@gmail.com>
2020-12-05 18:57:23 +1000

    добавили случайные снежинки.
    добавили случайные снежинки. осталось добавить фоновую музыку

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 
-let rowNumbers = 60;
+let rowNumbers = 40;
 let columnsNumbers = 100;
 let emptySymbhol = ' ';
-let fullSymbhol = '*';
-let fps = 5;
+let fullSymbhol = ['*', '+', '.'];
+let fps = 50;
 let arr = [];
 let snow = [];
 let pre = document.createElement("pre");
 let div = document.querySelector('div');
-let rnd = Math.floor(Math.random() * columnsNumbers);
+let rndColumn = Math.floor(Math.random() * columnsNumbers);
 let n = 0;
+let plus = 0;
+let stars = 0;
+let dot = 0;
 
 for (let i = 0; i < rowNumbers; i++){
     let string = [];
@@ -26,31 +29,43 @@ for (let i = 0; i < rowNumbers; i++){
     div.appendChild(pre);
     pre.appendChild(arr[i]);
 }
+function restart(){
+    n = 0;
+    rndColumn = Math.floor(Math.random() * columnsNumbers);
+    snow[n][rndColumn] = fullSymbhol[Math.floor(Math.random() * fullSymbhol.length)];
+    arr[n].textContent = snow[n].join('');   
+    if (snow[n][rndColumn] == '*'){
+        stars++;
+    }
+    if (snow[n][rndColumn] == '+'){
+        plus++;
+    }
+    if (snow[n][rndColumn] == '.'){
+        dot++;
+    }
+    console.log('stars = '+ stars + ' plus = '+ plus + ' dot = ' + dot);
+}
 
 function mainLoop(){        
     if (n < rowNumbers-1){
-        if (snow[n+1][rnd] == emptySymbhol){
-            snow[n+1][rnd] = fullSymbhol;
-            snow[n][rnd] = emptySymbhol;
+        if (snow[n+1][rndColumn] == emptySymbhol){
+            snow[n+1][rndColumn] = snow[n][rndColumn];
+            snow[n][rndColumn] = emptySymbhol;
             arr[n+1].textContent = snow[n+1].join(''); 
             arr[n].textContent = snow[n].join('');   
-            n++;
-            console.log(n);
+            n++;            
         }else{
-            n = 0;
-            rnd = Math.floor(Math.random() * columnsNumbers);
-            snow[n][rnd] = fullSymbhol;
-            arr[n].textContent = snow[n].join('');   
+            restart() 
         }
     }else {
-        n = 0;
-        rnd = Math.floor(Math.random() * columnsNumbers);
-        snow[n][rnd] = fullSymbhol;
-        arr[n].textContent = snow[n].join('');   
+        restart()
     }
 }
+const back = new Audio('back.it')
+function mySound(){back.play()};
 
 
 setInterval(mainLoop, fps);
+
 
 
